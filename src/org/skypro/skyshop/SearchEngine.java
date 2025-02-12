5243ae82565371430730f7a8875f48fe0d660b9a
Odissey2806 <aleksand2806@ya.ru>
2025-02-12 22:11:35 +0700

    Выполнил ДЗ Java Collections Framework: Set

--- src/org/skypro/skyshop/SearchEngine.java
+++ src/org/skypro/skyshop/SearchEngine.java
@@ -2,6 +2,7 @@ package org.skypro.skyshop;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.*;
 
 public class SearchEngine {
     private final List<Searchable> searchables = new ArrayList<>();
@@ -10,16 +11,46 @@ public class SearchEngine {
         searchables.add(searchable);
     }
 
-    public List<Searchable> findBestMatch(String search) throws BestResultNotFound {
-        List<Searchable> matches = new ArrayList<>();
+    public Map<String, Searchable> search(String query) {
+        Map<String, Searchable> results = new TreeMap<>();
         for (Searchable searchable : searchables) {
-            if (searchable != null && searchable.getSearchTerm().contains(search)) {
-                matches.add(searchable);
+            if (searchable != null && searchable.getSearchTerm().contains(query)) {
+                results.put(searchable.getName(), searchable);
             }
         }
-        if (matches.isEmpty()) {
+        return results;
+    }
+
+    public  Searchable findBestMatch (String search) throws  BestResultNotFound {
+        Searchable bestMatch = null;
+        int maxCount = 0;
+
+        for (Searchable searchable : searchables) {
+            if (searchable != null) {
+                int count = countOccurrences(searchable.getSearchTerm(), search);
+                if (count > maxCount) {
+                    maxCount = count;
+                    bestMatch = searchable;
+                }
+            }
+        }
+        if (bestMatch == null) {
             throw new BestResultNotFound("Не найдено подходящего результата для запроса: " + search);
         }
-        return matches;
+
+        return bestMatch;
+    }
+
+    private int countOccurrences(String str, String substring) {
+        int count = 0;
+        int index = 0;
+        int substringLength = substring.length();
+
+        while ((index = str.indexOf(substring, index)) != -1) {
+            count++;
+            index += substringLength;
+        }
+
+        return count;
     }
 }
--- src/org/skypro/skyshop/product/Article.java
+++ src/org/skypro/skyshop/product/Article.java
@@ -2,6 +2,8 @@ package org.skypro.skyshop.product;
 
 import org.skypro.skyshop.Searchable;
 
+import java.util.Objects;
+
 public class Article implements Searchable {
     private final String title;
     private final String content;
@@ -29,5 +31,17 @@ public class Article implements Searchable {
     @Override
     public String toString() {
         return getStringRepresentation();
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return Objects.equals(title, article.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title);
     }
 }
